edbdb718f3c95261090b6a2429b6477dcad62255
Edem Tsalah <edem.tsalah@gmail.com>
2014-06-09 22:52:53 +0000

    made users.html refer to the new grid control. Also set up the variables for the loading the grid on users.html page

--- js/users.js
+++ js/users.js
@@ -1,8 +1,33 @@
 $(function(){
 
-	var url = apiBaseUrl + "/users/";
+	var url = apiBaseUrl + "/users/",
+		userTypeControls = ['#usertype', '#uusertype'],
+		listUrl = apiBaseUrl + "/users",
+		countUrl = apiBaseUrl + "/users/meta/count",
+		startingPoint = 0,
+		limit = 100,
+		columns = [
+			{'name': 'id'}, {'name': 'memberid'}, 
+			{'name': 'username'}, {'name': 'usertype'}
+			],
+		identityColumn = "id",
+		extraControls = [
+			"<div class='btn-group'>","<button class='btn btn-mini edit-btn'>Edit</button>", 
+			"<button class='btn btn-mini delete-btn'>Delete</button>", "</div>"
+			],
+		gridTag = "#gridZ";
+
+	HideMsgBoxes(['#msg'])
 	getMembers($("#memberid"));
-	getUsertypes($("#usertype"));
+
+	createGrid(gridTag, listUrl, countUrl, startingPoint, limit, columns, 
+		identityColumn, null, extraControls);
+
+	for(var i = 0, k = userTypeControls.length; i < k; i++){
+		getUsertypes($(userTypeControls[i]));
+	}
+
+
 	$("#myTab a:first").tab('show');
 
 	$("#save").on('click', function(event){
@@ -18,8 +43,13 @@ $(function(){
 			url,
 			args
 		).done(function(args){
-			console.log('done');
+			showMsg('#msg', 'success', 'User has been added successfully');
+			$("#uname").val('');
+			$('#usertype').val('--User Type--');
+			$('#pword').val('');
+			$('#memberid').val('--Member--');
 		}).fail(function(args){
+			showMsg('#msg', 'error', "Couldn't add user");
 			console.log('failed');
 		});
 
